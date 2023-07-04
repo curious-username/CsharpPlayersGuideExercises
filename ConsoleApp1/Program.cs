46ace6483e60601266793f3608e28ef49c524a1e
unknown <bcalvi1@wgu.edu>
2023-07-03 19:02:29 -0700

    initial push

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq.Expressions;
+
+Console.WriteLine("What kind of thing are we talking about?");
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string description = Console.ReadLine();
+string thingDescription = " of Doom 3000";
+Console.WriteLine("The " + description + thingDescription + "!");
