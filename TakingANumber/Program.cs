2f83481d4a16dc2c3b3b92dbe093c7c2cf4acbaa
curious-username <bcalvi1@wgu.edu>
2023-07-08 17:37:30 -0700

    Taking a number challange is complete, taking a number still in works

---
+++ TakingANumber/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int text = AskForNumber("How old are you?");
+Console.WriteLine("You are " + text + " years old.");
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    Console.WriteLine($"{text} {min} and {max}");
+    while (true)
+    {
+        int playerAnswer = Convert.ToInt32(Console.ReadLine());
+        if (playerAnswer <= min)
+        {
+            Console.WriteLine("Too small, it must be between min and max values");
+        }
+        else if (playerAnswer >= max)
+        {
+            Console.WriteLine("Too large, it must be between min and max values");
+        }
+        else
+        {
+            
+            return playerAnswer;
+        }
+    }
+    
+}
+
+int answer = AskForNumberInRange("Please enter a number between ", 10, 20);
+Console.WriteLine($"You have entered {answer}");
