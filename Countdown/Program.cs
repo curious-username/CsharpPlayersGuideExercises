75107c2c13817242f0df4b5d4e742049e606ddd2
curious-username <bcalvi1@wgu.edu>
2023-07-08 18:02:32 -0700

    countdown complete

--- Countdown/Program.cs
+++ Countdown/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int CountDown(int number)
+{
+    if (number == 0) return 0;
+    Console.WriteLine(number);
+    return CountDown(number - 1);
+}
+
+Console.WriteLine("It's the final countdown!");
+CountDown(10);
+Console.WriteLine(0);
+Console.WriteLine("YEAAAHHHHHHHHH");
