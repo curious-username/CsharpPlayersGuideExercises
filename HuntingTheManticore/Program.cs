f439ba64e849d67e7c5c7473e5af68978ed08c01
curious-username <bcalvi1@wgu.edu>
2023-07-23 15:59:19 -0700

    Soup

---
+++ HuntingTheManticore/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+/*
+ * 
+ * Use different colors for different types of messages
+ * 
+// first user will pilot the manticore(10 health)
+    //secretly establish how far the Manticore from the city, range 0 to 100
+    //screen clears
+    
+
+//second user will play city defense (15 health)
+    //player will guess where the manticor is. if below the guessed number too short, higher than the number, too far, hit will take points away
+    //normal fire 1 point
+    //multiple of 3 fire blast, 3 dmg (%3)
+    //multiple of 5 electric blast, 5 dmg (%5)
+    //if both, 10 dmg
+    //if not distroyed, city 1 dmg
+*/
+
+
+
+int manticorLife = 10;
+int manticorRange;
+int cityHealth = 15;
+int currentTurn = 0;
+int playerGuess;
+int cannonDmg = 0;
+
+
+
+Console.WriteLine("Player 1, please enter how far the manitcor is");
+manticorRange = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Player 2, it is your turn");
+Console.WriteLine(new string('-', 20));
+
+while (true)
+{
+    currentTurn++;
+    
+    int CannonDmg(int currentRound)
+    {
+        int dmg;
+        if(currentRound % 3 == 0 || currentRound % 5 == 0)
+        {
+            dmg = 3;
+            return dmg;
+        }
+        else if(currentRound % 5 == 0 && currentRound % 3 == 0)
+        {
+            dmg = 10;
+            return dmg;
+        }
+        else {
+            dmg = 1;
+            return dmg;
+        }
+    }
+
+    cannonDmg = CannonDmg(currentTurn);
+
+    void CannonRoutine()
+    {
+        int cannonRange;    
+        cannonRange = Convert.ToInt32(Console.ReadLine());
+        if(cannonRange <= manticorRange) { }
+        // enter number
+        //return number
+        // if logic method
+
+        
+    }
+
+
+    void CityStatus()
+    {
+        Console.WriteLine($"STATUS: Round {currentTurn} City: {cityHealth}/15 Manticore: {manticorLife}/10");
+        Console.WriteLine($"The cannon is expected to deal {cannonDmg} this round");
+        Console.Write("Enter desired cannon range: ");
+        CannonRoutine();
+
+    }
+
+
+    
+}
+
+/*
+ SAMPLE UI:
+    Player 2, it is your turn
+    ----
+    STATUS: Round 1 City: 15/15 Manticore: 10/10
+    The cannon is expected to deal 1 damage this round
+    Enter desired cannon range: [player enters number]
+    That round [OVERSHOT, OR FELL SHORT] of the target. [OR] [was a DIRECT HIT].
+ */
---
+++ SimulaSoup/Program.cs
@@ -0,0 +1,69 @@
+﻿int food, seasoning, choice;
+int[] ingredients;
+
+
+
+void Recipe((food, ingredient, seasoning) soup)
+{
+
+}
+
+void MainMenu()
+{
+    Console.WriteLine("Welcome to the Soup Maker");
+    Console.WriteLine("Select one type, what ingredients you want, and the flavor you are achieving\n");
+    Console.WriteLine("Select an option to start your creation");
+    Console.WriteLine("1.) Food");
+    Console.WriteLine("2.) Ingredients");
+    Console.WriteLine("3.) Seasoning");
+    Console.WriteLine("Select an option");
+    choice = Convert.ToInt32(Console.ReadLine());
+    if (choice != 1 || choice != 2 || choice != 3)
+    {
+        Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
+        MainMenu();
+    }
+}
+
+MainMenu();
+
+void FoodMenu()
+{
+    Console.WriteLine("Please select a food option");
+    Console.WriteLine("1 for soup, 2 for stew and 3 for gumbo");
+    Console.WriteLine("What would you like to make?");
+    food = Convert.ToInt32(Console.ReadLine());
+    if(food != 1 || food != 2 || food != 3)
+    {
+        Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
+        FoodMenu();
+    }
+    else
+    {
+        MainMenu();
+    }
+}
+
+void IngredientsMenu()
+{
+    Console.WriteLine("Please select ingredients for your food");
+    Console.WriteLine("1 - Mushrooms, 2 - Chicken, 3 - Carrots, 4 - Potatoes 5 - Done");
+    Console.WriteLine("What would you like to add to your food?");
+    food = Convert.ToInt32(Console.ReadLine());
+    if (food != 1 || food != 2 || food != 3 || food != 4 || choice != 5)
+    {
+        Console.WriteLine("Please select a valid choice. Select 1, 2, 3, 4, 5 ");
+        IngredientsMenu();
+    }
+    else if(food == 5)
+    {
+        MainMenu();
+    }
+}
+
+
+enum food{soup, stew, gumbo}
+
+enum ingredient{mushrooms, chicken, carrots, potatoes}
+
+enum seasoning{spicy, salty, sweet}
