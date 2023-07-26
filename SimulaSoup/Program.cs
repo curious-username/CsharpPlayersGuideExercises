1d113cea3ed1394c2daf66af267bdccc86b8a6d5
curious-username <bcalvi1@wgu.edu>
2023-07-25 22:36:54 -0700

    fix if logic without catch-try

--- SimulaSoup/Program.cs
+++ SimulaSoup/Program.cs
@@ -1,5 +1,9 @@
-﻿int food, seasoning, choice;
-int[] ingredients;
+﻿string food;
+int seasoning = 0;
+
+int count = 0;
+
+int[] ingredientSelection = new int[5];
 
 
 
@@ -17,50 +21,107 @@ void MainMenu()
     Console.WriteLine("2.) Ingredients");
     Console.WriteLine("3.) Seasoning");
     Console.WriteLine("Select an option");
-    choice = Convert.ToInt32(Console.ReadLine());
-    if (choice != 1 || choice != 2 || choice != 3)
+    
+    //var choice = Console.ReadLine();
+    
+    if (Console.ReadLine() is string)
+    {
+        var finalChoice = Convert.ToInt32(choice);
+
+        if (finalChoice == 1) { FoodMenu(); }
+        //else if (choice == 2) { IngredientsMenu(); }
+        else if (finalChoice == 3) { SeasoningMenu(); }
+        else
+        {
+            Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
+        }
+    }
+    else
     {
         Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
         MainMenu();
     }
+
+    //Console.WriteLine(choice == 1);
+
+    //if (choice != 1 ||  choice != 2 || choice != 3)
+    //{
+    //    Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
+    //    MainMenu();
+    //}
+    //var finalChoice = Convert.ToInt32(choice);
+
+    
+
+    MainMenu();
 }
 
-MainMenu();
+    
 
 void FoodMenu()
 {
     Console.WriteLine("Please select a food option");
     Console.WriteLine("1 for soup, 2 for stew and 3 for gumbo");
     Console.WriteLine("What would you like to make?");
-    food = Convert.ToInt32(Console.ReadLine());
-    if(food != 1 || food != 2 || food != 3)
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1) { food = "soup."; }
+    else if (choice == 2) { food = "stew."; }
+    else if (choice == 3) { food = "gumbo"; }
+    else 
     {
-        Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
-        FoodMenu();
+    Console.WriteLine("Please select a valid choice. Select 1, 2, or 3 ");
+    FoodMenu();
     }
-    else
-    {
-        MainMenu();
-    }
+    
+    MainMenu();
 }
 
-void IngredientsMenu()
+//void IngredientsMenu()
+//{
+//    Console.WriteLine("Please select ingredients for your food");
+//    Console.WriteLine("1 - Mushrooms, 2 - Chicken, 3 - Carrots, 4 - Potatoes 5 - Done");
+//    Console.WriteLine("What would you like to add to your food?");
+//    choice = Convert.ToInt32(Console.ReadLine());
+//    if (choice != 1 || choice != 2 || choice != 3 || choice != 4 || choice != 5)
+//    {
+//        Console.WriteLine("Please select a valid choice. Select 1, 2, 3, 4, 5 ");
+//        IngredientsMenu();
+//    }
+//    else if (choice < 5)
+//    {
+//        for (int i = 0; i < ingredientSelection.Length; i++)
+//        {
+//            if (i != 0)
+//            {
+//                Console.WriteLine($"You have already selected{choice}");
+//                Console.WriteLine(i);
+//                break;
+//            }
+//            else
+//            {
+//                ingredientSelection[count] = choice;
+//                count++;
+//                break;
+//            }
+//        }
+//    }
+//    else if (food == 5)
+//    {
+//        MainMenu();
+//    }
+//}
+
+void SeasoningMenu()
 {
-    Console.WriteLine("Please select ingredients for your food");
-    Console.WriteLine("1 - Mushrooms, 2 - Chicken, 3 - Carrots, 4 - Potatoes 5 - Done");
-    Console.WriteLine("What would you like to add to your food?");
-    food = Convert.ToInt32(Console.ReadLine());
-    if (food != 1 || food != 2 || food != 3 || food != 4 || choice != 5)
-    {
-        Console.WriteLine("Please select a valid choice. Select 1, 2, 3, 4, 5 ");
-        IngredientsMenu();
-    }
-    else if(food == 5)
-    {
-        MainMenu();
-    }
+    Console.WriteLine("Building out Seasoning Menu");
+    /*
+     * TODO: BUILD THIS
+     */
+    MainMenu();
 }
 
+MainMenu();
 
 enum food{soup, stew, gumbo}
 
