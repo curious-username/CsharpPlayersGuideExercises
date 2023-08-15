ad150abf70abb2b37d62eb36019952004603e64e
curious-username <bcalvi1@wgu.edu>
2023-08-14 23:29:32 -0700

    fletcher redo, working on understanding solution

--- VinFlectchersArrows/Program.cs
+++ VinFlectchersArrows/Program.cs
@@ -1,133 +1,201 @@
-﻿bool _isFirstTimeRunning = true;
-float totalCost = 0.0f;
-// need to use the method in the class to calculate 
+﻿//class and method structure ONLY!!!
+//Define an arrow class, fields for arrowhead, fletching, length(arrowhead and fletching are good enums)
+//allow user to pick arrowhead, fletching, and length
+//getcost method to return float 
 
-void MainMenu()
+
+
+
+
+
+
+
+
+
+
+
+
+Arrowhead GetArrowheadType()
 {
-    if (_isFirstTimeRunning)
+    Console.WriteLine("Arrowhead type (steel, wood, obsidian): ");
+    string input = Console.ReadLine();
+    return input switch
     {
-        Console.WriteLine("I am Vin Fletcher, a skilled arrow maker.");
-        _isFirstTimeRunning=false;
+        "steel" => Arrowhead.steel,
+        "wood" => Arrowhead.wood,
+        "obsidian" => Arrowhead.obsidian,
+        _ => throw new Exception("Please select steel, wood, or obsidian")
+    };
+}
+
+Fletching GetFletchingType()
+{
+    Console.WriteLine("Select a fletching type of plastic, turkey feathers, or goose feathers");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "plastic" => Fletching.plastic,
+        "turkey feathers" => Fletching.turkeyFeathers,
+        "goose feathers" => Fletching.gooseFeathers,
+        _ => throw new Exception("Please select plastic, turkey feathers, or good feathers")
+    };
+}
+
+float GetLength()
+{
+    float length = 0;
+    while (length < 60 || length > 100)
+    {
+        Console.WriteLine("Select a length for your arrow between 60 and 100 meters");
+        length = Convert.ToSingle(Console.ReadLine());
+
     }
-    Console.WriteLine("The arrow has three parts. The arrowhead, shaft, and fletching.");
-    ArrowMaker();
-
-
+    return length;
 }
 
-
-string ArrowHead()
-{
-    string arrowhead;
-
-    Console.WriteLine("Starting with the arrowhead what do you want?");
-    Console.WriteLine("1 for steel, 2 for wood, or 3 for obsidian");
-    arrowhead = Console.ReadLine();
-    if (arrowhead == "1") { arrowhead = "Steel"; totalCost += 10.0f; }
-    else if (arrowhead == "2") { arrowhead = "Wood"; totalCost += 3.0f; }
-    else if (arrowhead == "3") { arrowhead = "Obsidian";totalCost += 5.0f; }
-    else
-    {
-        Console.WriteLine("Please make a valid selection");
-        ArrowHead();
-    }
-    
-    return arrowhead;
-}
-
-int Shaft()
-{
-    float shaft;
-    
-
-    Console.WriteLine("Next we select the shaft length.");
-    Console.WriteLine("Enter a value between 60 and 100");
-    do
-    {
-        string answer = Console.ReadLine();
-        if (Convert.ToInt32(answer) > 100 || Convert.ToInt32(answer) < 60)
-        {
-            Console.WriteLine("Please enter a value between 60 and 100");
-        }
-        else
-        {
-            shaft = Convert.ToInt32(answer);
-            totalCost += shaft * 0.05f;
-            break;
-        }
-    } while (true);
-    
-    return Convert.ToInt32(shaft);
-}
-
-string Fletching()
-{
-    string fletching;
-
-    Console.WriteLine("Lastly select a fletching");
-    Console.WriteLine("1 for Plastic, 2 for Turkey Feathers, or 3 for Goose Feathers");
-    fletching = Console.ReadLine();
-    if (fletching == "1") { fletching = "Plastic"; totalCost += 10.0f; }
-    else if (fletching == "2") { fletching = "Turkey Feathers"; totalCost += 5.0f; }
-    else if (fletching == "3") { fletching = "Goose Feathers"; totalCost += 3.0f; }
-    else
-    {
-        Console.WriteLine("Please make a valid selection");
-        ArrowHead();
-    }
-    return fletching;
-}
-
-void ArrowMaker()
-{
-    Arrow a = new Arrow(ArrowHead(), Shaft(), Fletching());
-
-    Console.WriteLine(a.GetArrowhead());
-    Console.WriteLine(a.GetFletching());
-    Console.WriteLine(a.GetShaft());
-
-
-
-}
-ArrowMaker();
 class Arrow
 {
-    private string arrowhead = "undefined";
-    private int shaft = 0;
-    private string fletching = "undefined";
+    public Arrowhead _arrowhead;
+    public Fletching _fletching;
+    public float _arrowLength;
 
-
-    public Arrow(string arrowhead, int shaft, string fletching)
+    Arrow(Arrowhead arrowhead, Fletching fletching, float arrowLength)
     {
-        this.arrowhead = arrowhead;
-        this.shaft = shaft;
-        this.fletching = fletching;
+        _arrowhead = arrowhead;
+        _fletching = fletching;
+        _arrowLength = arrowLength;
     }
 
-    public string GetArrowhead() => arrowhead; 
-    public int GetShaft() => shaft;
-    public string GetFletching() => fletching;
-
-    public void PrintMessage()
+    float GetCost()
     {
-        Console.WriteLine($"You have created an arrow with {this.arrowhead}, {this.shaft}, {this.fletching}");
+        float arrowheadCost = _arrowhead switch
+        {
+            Arrowhead.steel => 10,
+            Arrowhead.wood => 3,
+            Arrowhead.obsidian => 5
+        };
+
+        float fletchingCost = _fletching switch
+        {
+            Fletching.plastic => 10,
+            Fletching.turkeyFeathers => 5,
+            Fletching.gooseFeathers => 3
+        };
+
+        float shaftCost = 0.05f * _arrowLength;
+        
+        return arrowheadCost + fletchingCost + shaftCost;
     }
-
-    public float GetPrice(float total)
-    {
-        Console.WriteLine($"Your total price is ");
-        return 1.1f;
-    }
-    
-
-        /*
-         * arrowheads: steel = 10 gold, wood costs 3 gold, obsidian costs 5 gold, 
-         * shaft: 0.05 gold per cm
-         * fletching: turkey feathers = 10 gold, turkey feathers = 5 gold, goose feathers = 3 gold
-         * three seperate methods to build total, use get cost to make the calculation
-         */
-
-
-    
 }
 
+
+
+/*
+ * arrowhead - steel:10 gold, wood: 3 gold, obsidian: 5gold
+ * fletching - plastic: 10 gold, turkey feathers: 5gold, goose feathers: 3 gold
+ * shaft - 0.05g gold per centimeter(between 60 and 100m)
+ */
+
+
+
+
+
+
+
+
+enum Arrowhead { steel, wood, obsidian };
+enum Fletching { plastic, turkeyFeathers, gooseFeathers};
+
+
+//bool _isFirstTimeRunning = true;
+//float totalCost = 0.0f;
+//// need to use the method in the class to calculate 
+
+//Arrow arrow = GetArrow();
+//Console.WriteLine($"That arrow costs {arrow.GetCost()} gold.");
+
+
+
+//Arrow GetArrow()
+//{
+//Arrowhead arrowhead = GetArrowheadType();
+//Fletching fletching = GetFletchingType();
+//float length = GetLength();
+
+//return new Arrow(arrowhead, fletching, length);
+//}
+
+//Arrowhead GetArrowheadType()
+//{
+//Console.Write("Arrowhead type (steel, wood, obsidian): ");
+//string input = Console.ReadLine();
+//return input switch
+//{
+//"steel" => Arrowhead.Steel,
+//"wood" => Arrowhead.Wood,
+//"obsidian" => Arrowhead.Obsidian
+//};
+//}
+
+//Fletching GetFletchingType()
+//{
+//Console.Write("Fletching type (plastic, turkey feather, goose feather): ");
+//string input = Console.ReadLine();
+//return input switch
+//{
+//"plastic" => Fletching.Plastic,
+//"turkey feather" => Fletching.TurkeyFeathers,
+//"goose feather" => Fletching.GooseFeathers
+//};
+//}
+
+//float GetLength()
+//{
+//float length = 0;
+
+//while (length < 60 || length > 100)
+//{
+//Console.Write("Arrow length (between 60 and 100): ");
+//length = Convert.ToSingle(Console.ReadLine());
+//}
+
+//return length;
+//}
+
+//class Arrow
+//{
+//    public Arrowhead _arrowhead;
+//    public Fletching _fletching;
+//    public float _length;
+
+//    public Arrow(Arrowhead arrowhead, Fletching fletching, float length)
+//    {
+//        _arrowhead = arrowhead;
+//        _fletching = fletching;
+//        _length = length;
+//    }
+
+//    public float GetCost()
+//    {
+//        float arrowheadCost = _arrowhead switch
+//        {
+//            Arrowhead.Steel => 10,
+//            Arrowhead.Wood => 3,
+//            Arrowhead.Obsidian => 5
+//        };
+
+//        float fletchingCost = _fletching switch
+//        {
+//            Fletching.Plastic => 10,
+//            Fletching.TurkeyFeathers => 5,
+//            Fletching.GooseFeathers => 3
+//        };
+
+//        float shaftCost = 0.05f * _length;
+
+//        return arrowheadCost + fletchingCost + shaftCost;
+//    }
+//}
+
+//enum Arrowhead { Steel, Wood, Obsidian }
+//enum Fletching { Plastic, TurkeyFeathers, GooseFeathers }
